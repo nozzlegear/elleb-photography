bf9844319cc55396363f940b38f3a4e9bd366c6a
Joshua Harms <joshua@nozzlegear.com>
2025-08-01 10:08:52 -0500

    Add medusa/products.ts entrypoint

--- vite.config.mjs
+++ vite.config.mjs
@@ -36,7 +36,7 @@ export default defineConfig({
     rollupOptions: {
       input: getEntries([
         'js/main.ts',
-        'js/medusa/index.ts',
+        'js/medusa/products.ts',
         'css/main.css',
         'css/amp.css'
       ]),
